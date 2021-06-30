c2762c94a4cc41fb9649f71dc82078f8a17c391f
joaldojunior <joaldo.junior@ucsal.edu.br>
2021-06-30 13:57:21 -0300

    implementacao da pagina de detalhe do livro

--- casa-codigo/src/main/java/br/com/zup/desafio/casacodigo/controller/LivroController.java
+++ casa-codigo/src/main/java/br/com/zup/desafio/casacodigo/controller/LivroController.java
@@ -1,16 +1,19 @@
 package br.com.zup.desafio.casacodigo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.desafio.casacodigo.dto.DetalhesLivroDTO;
 import br.com.zup.desafio.casacodigo.dto.LivroDTO;
 import br.com.zup.desafio.casacodigo.dto.LivroFormDTO;
 import br.com.zup.desafio.casacodigo.model.Livro;
@@ -42,11 +45,22 @@ public class LivroController {
 		return ResponseEntity.ok().body(lista);
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesLivroDTO> detalhar(@PathVariable Long id){
+		Optional<Livro> livro = livroRepository.findById(id);
+		if(livro.isPresent()) {
+			return ResponseEntity.ok(new DetalhesLivroDTO(livro.get()));
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 	@PostMapping
 	public ResponseEntity<LivroFormDTO> inserir(@RequestBody @Valid LivroDTO livroDto) {
 		Livro livro = livroDto.converter(categoriaRepository, autorRepository);
 		livroRepository.save(livro);
 		return ResponseEntity.ok().body(new LivroFormDTO(livro));
 	}
+	
+	
 
 }
